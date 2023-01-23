3669d6ab33659aa502ad757965e6a9c0cc655c53
wundeeh <jakiriksh@gmail.com>
2023-01-23 23:41:26 +0000

    remove all but vital functionality

--- classes/question.rb
+++ classes/question.rb
@@ -12,37 +12,4 @@ class Question
     @num2 = rand(1..20)
     @answer = @num1 + @num2
   end
-
-  def start
-    answer = @answer
-    puts "What does #{@num1} + #{@num2} equal?"
-
-    print "> "
-    choice = $stdin.gets.chomp
-
-    if choice.to_i === answer
-      puts "Correct!"
-      exit(0)
-      # placeholder, should switch to next player
-    else
-      puts "Seriously? No!"
-      exit(0)
-      # placeholder, should switch to next player
-    end
-  end
 end
-
-quest = Question.new
-
-quest.start
-
-
-
-
-# test = Question.new
-
-# puts test.num1
-# puts "+"
-# puts test.num2
-# puts "="
-# puts test.answer
