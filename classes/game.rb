27ec954f5dd2bd95c583b385cb644997169b1b14
wundeeh <jakiriksh@gmail.com>
2023-01-23 22:09:50 +0000

    initial commit

---
+++ classes/game.rb
@@ -0,0 +1,16 @@
+class MainGame
+  # contains the questions and the players
+  # so that the two classes can interact
+  # with each other
+
+  # will transfer from player to player
+  # after each question
+
+  def start
+
+  end
+
+  def switch_player
+    
+  end
+end
