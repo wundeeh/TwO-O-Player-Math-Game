07d03745839984ad2086e85efac4fa5463efc579
wundeeh <jakiriksh@gmail.com>
2023-01-23 23:41:47 +0000

    remove unused variable

--- classes/player.rb
+++ classes/player.rb
@@ -2,31 +2,15 @@ class Player
   # stores the starting player info
   # has the individual player health
 
-  attr_reader :health
-
-  attr_accessor :player_name, :player_two
+  attr_accessor :name, :health
 
   def initialize
-    @player_name = ""
+    @name = ""
     @health = 3
   end
 
   def start
     print "Player Name> "
-    @player_name = $stdin.gets.chomp
+    @name = $stdin.gets.chomp
   end
 end
-
-player = Player.new
-
-player2 = Player.new
-
-player.start
-
-player2.start
-
-puts player.player_name
-puts player.health
-
-puts player2.player_name
-puts player2.health
